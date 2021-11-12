271619d94214d28ff1c22c99877a17ca130f5f64
meetkhatri44 <92850616+meetkhatri44@users.noreply.github.com>
2021-11-12 16:19:08 +0530

    Stop bad words

---
+++ main.py
@@ -0,0 +1,11 @@
+# importing package
+from better_profanity import profanity
+
+# text to be censored
+text = "What the shit are you doing?"
+
+# do censoring
+censored = profanity.censor(text)
+
+# view output
+print(censored)
